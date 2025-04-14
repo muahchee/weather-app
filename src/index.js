123ad2fa291abeaef86ce1ddabfcf5adad7ee9db
muahchee <187490843+muahchee@users.noreply.github.com>
2025-04-14 23:06:14 +1200

    add try catch

--- src/index.js
+++ src/index.js
@@ -1,30 +1,39 @@
 import "./styles.css";
 
 async function getWeather(location) {
-  const response = await fetch(
-    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=3RE8URH9BP6H4YL46EP2U53LT`,
-    { mode: "cors" },
-  );
+  try {
+    const response = await fetch(
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=3RE8URH9BP6H4YL46EP2U53LT`,
+      { mode: "cors" },
+    );
 
-  const weatherData = await response.json();
-  console.log(weatherData);
-  return weatherData;
+    const weatherData = await response.json();
+    console.log(weatherData);
+    return weatherData;
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 async function getCurrentConditions(location) {
-  const weatherData = await getWeather(location);
+  try {
+    const weatherData = await getWeather(location);
 
-  const currentObj = {
-    icon: weatherData.currentConditions.icon,
-    datetime: weatherData.currentConditions.datetime,
-    conditions: weatherData.currentConditions.conditions,
-    temp: weatherData.currentConditions.temp,
-    uvindex: weatherData.currentConditions.uvindex,
-    precipprob: weatherData.currentConditions.precipprob
+    const currentObj = {
+      icon: weatherData.currentConditions.icon,
+      datetime: weatherData.currentConditions.datetime,
+      conditions: weatherData.currentConditions.conditions,
+      temp: weatherData.currentConditions.temp,
+      uvindex: weatherData.currentConditions.uvindex,
+      precipprob: weatherData.currentConditions.precipprob,
+    };
+
+    console.log(weatherData.currentConditions);
+    console.log(currentObj);
+    return currentObj;
+  } catch (err) {
+    console.log(err);
   }
-
-  console.log(weatherData.currentConditions)
-  console.log(currentObj)
 }
 
 getCurrentConditions("Auckland");
