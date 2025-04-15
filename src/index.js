1ef54682e70cfee2e4891bd1020321f97c4592da
muahchee <187490843+muahchee@users.noreply.github.com>
2025-04-15 18:59:00 +1200

    made processing functions

--- src/index.js
+++ src/index.js
@@ -15,6 +15,21 @@ async function getWeather(location) {
   }
 }
 
+async function getGeneralInfo(location) {
+  try {
+    const currentLocation = await getWeather(location);
+
+    const generalObj = {
+      resolvedAddress: currentLocation.resolvedAddress,
+      description: currentLocation.description,
+    };
+    console.log(generalObj);
+    return generalObj;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
 async function getCurrentConditions(location) {
   try {
     const weatherData = await getWeather(location);
@@ -28,7 +43,6 @@ async function getCurrentConditions(location) {
       precipprob: weatherData.currentConditions.precipprob,
     };
 
-    console.log(weatherData.currentConditions);
     console.log(currentObj);
     return currentObj;
   } catch (err) {
@@ -36,4 +50,49 @@ async function getCurrentConditions(location) {
   }
 }
 
-getCurrentConditions("Auckland");
+async function getDaysForcast(location) {
+  try {
+    const weatherData = await getWeather(location);
+
+    const sevenDayData = weatherData.days.splice(0, 7);
+
+    const sevenDayArr = [];
+
+    sevenDayData.forEach((obj) => {
+      let dayObj = {
+        datetime: obj.datetime,
+        description: obj.description,
+        icon: obj.icon,
+        temp: obj.temp,
+        tempmax: obj.tempmax,
+        tempmin: obj.tempmin,
+        uvindex: obj.uvindex,
+        precipprob: obj.precipprob,
+      };
+
+      let hoursArr = [];
+      obj.hours.forEach((obj) => {
+        let hourObj = {
+          datetime: obj.datetime,
+          icon: obj.icon,
+          temp: obj.temp,
+          precipprob: obj.precipprob,
+        };
+        hoursArr.push(hourObj);
+      });
+      dayObj["hours"] = hoursArr;
+
+      sevenDayArr.push(dayObj);
+    });
+
+    console.log(sevenDayArr);
+
+    return;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+// getGeneralInfo("Auckalnd");
+// getCurrentConditions("Auckland");
+getDaysForcast("Auckland");
